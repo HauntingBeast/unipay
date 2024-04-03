b9b291af92f26eecd3d055a747b72443205b0d5b
HauntingBeast <piyushkedia819@gmail.com>
2024-04-03 23:08:02 +0530

    2nd commit

--- routes/unipay.js
+++ routes/unipay.js
@@ -6,12 +6,12 @@ const {testing,signup,signin,about,update,auth,balance,transfer}=require('../con
 
 router.route('/signup').get(testing).post(signup)
 router.route('/signin').get(testing).post(signin)
-router.route('/user').get(testing)
-router.route('/user/getBalance').get(testing)
+// router.route('/user').get(testing) (frontend)
+// router.route('/user/getBalance').get(testing) (ahref)
 router.route('/user/update').get(testing).put(auth,update)
 router.route('/user/about').get(about)
-router.route('/user/transfer').get(testing)
-router.route('/user/notification').get(testing)
+// router.route('/user/transfer').get(testing) (ahref)
+// router.route('/user/notification').get(testing) (aws)
 router.route('/account/balance').get(auth,balance)
 router.route('/account/transfer').get(testing).post(auth,transfer)
 
